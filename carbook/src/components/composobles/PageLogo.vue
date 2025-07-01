1074d737e89f9936e65a0b456bf9cd922e553d8e
jasna18 <jasnamnaz@gmail.com>
2025-07-01 09:26:04 +0400

    fc

---
+++ carbook/src/components/composobles/PageLogo.vue
@@ -0,0 +1,22 @@
+<template>
+  <router-link to="/">
+    <h1>
+        <span>
+            CAR
+        </span>
+        <span>
+            BOOK
+            </span>
+    </h1>
+    </router-link>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ carbook/src/components/layout/PageHeader.vue
@@ -0,0 +1,22 @@
+<template>
+<header>
+    <div>    
+        <PageLogo/>
+   </div>
+</header>
+</template>
+
+<script>
+
+import PageLogo from '../composobles/PageLogo.vue';
+
+export default {
+    components: {
+        PageLogo
+    },
+}
+</script>
+
+<style>
+
+</style>
