72b626a3f5de794743832f9b981f510fa8d3525c
jasna18 <jasnamnaz@gmail.com>
2025-07-02 09:58:15 +0400

    pagelogo added scroll and mobileview

--- carbook/src/components/composobles/PageLogo.vue
+++ carbook/src/components/composobles/PageLogo.vue
@@ -1,22 +1,40 @@
 <template>
   <router-link to="/">
-    <h1>
-        <span>
-            CAR
-        </span>
-        <span>
-            BOOK
-            </span>
+    <h1 class="text-xl font-bold cursor-pointer">
+        <span :class="carClass">CAR</span>
+        <span>BOOK</span>
     </h1>
     </router-link>
 </template>
 
 <script>
+import { computed, onBeforeMount,onMounted,ref } from 'vue';
 export default {
+    setup() {
+        const isScrolled = ref(false);
+        const isMobile = ref(window.innerWidth < 760);
+        const handleScroll = () => {
+            isScrolled.value = window.scrollY > 50;
+        };
+        onMounted(() => {
+            window.addEventListener('scroll', handleScroll);
+            });
+            onBeforeMount(() => {
+            window.removeEventListener('scroll', handleScroll);
+            });
+            const carClass = computed( () =>{
+                if(isMobile.value) return "text-black";
+                return isScrolled.value ? "text-black" : "text-white";
 
+            });
+            return {
+                carClass
+            }
+    
+    }
 }
 </script>
 
 <style>
 
-</style>
+</style>                                                       b 
