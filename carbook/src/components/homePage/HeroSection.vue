3f24eda86e9b53ebf70d5d4e21efeea2c83eac98
jasna18 <jasnamnaz@gmail.com>
2025-07-10 10:11:54 +0400

    added homePage folder

---
+++ carbook/src/components/homePage/HeroSection.vue
@@ -0,0 +1,12 @@
+<script setup>
+import bgImage from "../../assets/images/bg_1.png";
+</script>
+
+<template>
+    <section class="bg-cover relative h-screen" :style="{ backgroundImage: `url(${bgImage})` }">
+<div class="container">
+    <h1 class="text-center">Welcome to my website</h1>
+        <p class="text-center">This is a simple hero section with a background image.</p>
+</div>
+    </section>
+</template>
