e59da80119c4180ad31992678a111cd9ba094c77
jasna18 <jasnamnaz@gmail.com>
2025-07-10 10:19:32 +0400

    made background image fit to full screen

--- carbook/src/components/homePage/HeroSection.vue
+++ carbook/src/components/homePage/HeroSection.vue
@@ -3,10 +3,20 @@ import bgImage from "../../assets/images/bg_1.png";
 </script>
 
 <template>
-    <section class="bg-cover relative h-screen" :style="{ backgroundImage: `url(${bgImage})` }">
+    <section class="bg-cover" :style="{ backgroundImage: `url(${bgImage})` }">
 <div class="container">
     <h1 class="text-center">Welcome to my website</h1>
-        <p class="text-center">This is a simple hero section with a background image.</p>
+    <p class="text-center">This is a simple hero section with a background image.</p>
+
 </div>
     </section>
 </template>
+    <style scoped>
+    section.bg-cover {
+        background-size: cover;
+        background-position: center;
+        background-repeat: no-repeat;
+        min-height: 100vh;
+        width: 100vw;
+    }
+    </style>
