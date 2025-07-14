89b2d4d0ae99e40b52d18de89a192fd6fbe33090
jasna18 <jasnamnaz@gmail.com>
2025-07-14 13:01:42 +0400

    nav links and corresponding router links added

--- carbook/src/router/index.js
+++ carbook/src/router/index.js
@@ -1,6 +1,13 @@
 import {createRouter, createWebHistory} from 'vue-router'
 import DefaultLayout from '../components/layout/DefaultLayout.vue'
 import HomePage from '../page/HomePage.vue'
+import AboutPage from '../page/AboutPage.vue'
+import CarsPage from '../page/CarsPage.vue'
+import BlogPage from '../page/BlogPage.vue'
+import PricingPage from '../page/PricingPage.vue'
+import ServicesPage from '../page/ServicesPage.vue'
+import ContactPage from '../page/ContactPage.vue'
+
 
 const routes = [
     {
@@ -11,6 +18,36 @@ const routes = [
             path:"",
             name:"home",
             component: HomePage
+            },
+               {
+            path:"/about",
+            name:"about",
+            component: AboutPage
+            },
+             {
+            path:"/blog",
+            name:"blog",
+            component: BlogPage
+            },
+             {
+            path:"/cars",
+            name:"cars",
+            component: CarsPage
+            },
+             {
+            path:"/pricing",
+            name:"Pricing",
+            component: PricingPage
+            },
+             {
+            path:"/services",
+            name:"services",
+            component: ServicesPage
+            },
+             {
+            path:"/contact",
+            name:"contact",
+            component: ContactPage
             },
 ],
     },
