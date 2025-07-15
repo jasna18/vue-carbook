67d50af89ca9d1b1d20427233fc06d90bf4f3e55
jasna18 <jasnamnaz@gmail.com>
2025-07-15 12:54:38 +0400

    added Featuredsection vue page

---
+++ carbook/src/components/homePage/FeaturedSection.vue
@@ -0,0 +1,12 @@
+<template>
+  <SectionHeading  description = "what we offer" title="Featured Vehicles"/>
+</template>
+
+<script setup>
+import SectionHeading from '../composobles/SectionHeading.vue';
+
+</script>
+
+<style>
+
+</style>
