2db182bce13e9f02da288aad9fc6c5f9b559dca9
jasna18 <jasnamnaz@gmail.com>
2025-07-15 12:30:33 +0400

    added featured section from homepage

--- carbook/src/page/HomePage.vue
+++ carbook/src/page/HomePage.vue
@@ -1,12 +1,14 @@
 <template>
   <div>
     <HeroSection/>
+    <FeaturedSection/>
   </div>
 </template>
 
 <script setup>
 
 import HeroSection from '../components/homePage/HeroSection.vue';
+import FeaturedSection from '../components/homePage/FeaturedSection.vue';
 
 </script>
 
