5e780c157d03eca0373c893d7ecb67ca847f9862
jasna18 <jasnamnaz@gmail.com>
2025-07-16 14:15:01 +0400

    formated featured section

--- carbook/src/components/homePage/FeaturedSection.vue
+++ carbook/src/components/homePage/FeaturedSection.vue
@@ -1,12 +1,24 @@
 <template>
-  <SectionHeading  description = "what we offer" title="Featured Vehicles"/>
+  <div>
+    <SectionHeading description="what we offer" title="Featured Vehicles" />
+    <div v-for="(car, index) in cars" :key="index">
+      <!-- props from carcard -->
+      <CarCard
+        :id="car.id"
+        :name="car.name"
+        :brand="car.brand"
+        :price="car.price"
+        :image="car.image"
+      />
+    </div>
+    <!-- Removed extra CarCards component outside the loop -->
+  </div>
 </template>
 
 <script setup>
-import SectionHeading from '../composobles/SectionHeading.vue';
-
+import SectionHeading from "../composobles/SectionHeading.vue";
+import CarCard from "../cards/CarCard.vue";
+import { cars } from "../../data/carsData.js";
 </script>
 
-<style>
-
-</style>
+<style></style>
