08e186bbe28c2ea5396b4dad982e20114d03ed82
jasna18 <jasnamnaz@gmail.com>
2025-07-16 14:01:31 +0400

    formatted herosection

--- carbook/src/components/homePage/HeroSection.vue
+++ carbook/src/components/homePage/HeroSection.vue
@@ -3,31 +3,38 @@ import bgImage from "../../assets/images/bg_1.png";
 </script>
 
 <template>
-    <section class="bg-cover relative h-screen" :style="{ backgroundImage: `url(${bgImage})` }">
-<div class="absolute inset-0 bg-black opacity-25"></div>
-    <div class="relative flex items-center justify-center h-full text-center text-white px-4">
+  <section
+    class="bg-cover relative h-screen"
+    :style="{ backgroundImage: `url(${bgImage})` }"
+  >
+    <div class="absolute inset-0 bg-black opacity-25"></div>
+    <div
+      class="relative flex items-center justify-center h-full text-center text-white px-4"
+    >
       <div class="w-full md:w-3/5">
-        <h1 class="text-4xl md:text-5xl font-bold mb-4">Fast & Easy way to rent a car</h1>
+        <h1 class="text-4xl md:text-5xl font-bold mb-4">
+          Fast & Easy way to rent a car
+        </h1>
         <p class="text-md md:text-xl mb-6">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut 
-          labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
-           
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris
         </p>
         <h2 class="text-2xl font-semibold">Easy steps for renting a car</h2>
       </div>
     </div>
-    </section>
+  </section>
 </template>
 <style scoped>
 section.bg-cover {
-    /* Custom CSS for fallback or extra styles */
-    background-size: cover;
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
-    background-position: center;
-    background-repeat: no-repeat;
-    min-height: 100vh;
-    width: 100vw;
+  /* Custom CSS for fallback or extra styles */
+  background-size: cover;
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  background-position: center;
+  background-repeat: no-repeat;
+  min-height: 100vh;
+  width: 100vw;
 }
 </style>
