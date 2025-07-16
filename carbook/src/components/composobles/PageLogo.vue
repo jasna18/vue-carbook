1a799c371f12ebcacbd5eeb45e8cd6e3c29df7cb
jasna18 <jasnamnaz@gmail.com>
2025-07-16 13:58:07 +0400

    formatted pagelogo

--- carbook/src/components/composobles/PageLogo.vue
+++ carbook/src/components/composobles/PageLogo.vue
@@ -1,48 +1,43 @@
 <template>
   <router-link to="/">
     <h1 class="text-xl font-bold cursor-pointer">
-        <span :class="carClass">CAR</span>
-        <span class="text-green-500" >BOOK</span>
+      <span :class="carClass">CAR</span>
+      <span class="text-green-500">BOOK</span>
     </h1>
-    </router-link>
+  </router-link>
 </template>
 
 <script>
-import { computed, onBeforeMount,onMounted,ref } from 'vue';
+import { computed, onBeforeMount, onMounted, ref } from "vue";
 export default {
-    setup() {
-        const isScrolled = ref(false);
-        const isMobile = ref(window.innerWidth < 760);
+  setup() {
+    const isScrolled = ref(false);
+    const isMobile = ref(window.innerWidth < 760);
 
-        const handleScroll = () => {
-            isScrolled.value = window.scrollY > 50;
-        };
-        const handleResize = () => {
-            isMobile.value = window.innerWidth < 760;
-        };
-        onMounted(() => {
-            window.addEventListener('scroll', handleScroll);
-            window.addEventListener('resize', handleResize);
-
-            });
-            onBeforeMount(() => {
-            window.removeEventListener('scroll', handleScroll);
-            window.removeEventListener('resize', handleResize);
-
-            });
-            const carClass = computed( () =>{
-                if(isMobile.value) return "text-black";
-                return isScrolled.value ? "text-black" : "text-white";
-
-            });
-            return {
-                carClass
-            }
-    
-    }
-}
+    const handleScroll = () => {
+      isScrolled.value = window.scrollY > 50;
+    };
+    const handleResize = () => {
+      isMobile.value = window.innerWidth < 760;
+    };
+    onMounted(() => {
+      window.addEventListener("scroll", handleScroll);
+      window.addEventListener("resize", handleResize);
+    });
+    onBeforeMount(() => {
+      window.removeEventListener("scroll", handleScroll);
+      window.removeEventListener("resize", handleResize);
+    });
+    const carClass = computed(() => {
+      if (isMobile.value) return "text-black";
+      return isScrolled.value ? "text-black" : "text-white";
+    });
+    return {
+      carClass,
+    };
+  },
+};
 </script>
 
-<style>
-
-</style>                                                       b 
+<style></style>
+b
