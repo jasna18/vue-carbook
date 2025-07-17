f49ac81974cc6e4d050657d4db603d98f6fd59d4
jasna18 <jasnamnaz@gmail.com>
2025-07-17 08:26:59 +0400

    added aboutUs in homepage component

--- carbook/src/page/HomePage.vue
+++ carbook/src/page/HomePage.vue
@@ -1,18 +1,15 @@
 <template>
   <div>
-    <HeroSection/>
-    <FeaturedSection/>
+    <HeroSection />
+    <FeaturedSection />
+    <AboutUs />
   </div>
 </template>
 
 <script setup>
-
-import HeroSection from '../components/homePage/HeroSection.vue';
-import FeaturedSection from '../components/homePage/FeaturedSection.vue';
-
+import HeroSection from "../components/homePage/HeroSection.vue";
+import FeaturedSection from "../components/homePage/FeaturedSection.vue";
+import AboutUs from "../components/homePage/AboutUs.vue";
 </script>
 
 <style></style>
-
-
-
